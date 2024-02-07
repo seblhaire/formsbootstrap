6e90ae756dcfd66296b85fd9a1b79d25b008025a
Sébastien L'haire <sebastien@lhaire.org>
2024-02-07 20:24:04 +0100

    remove collective package: first steps

---
+++ src/FormsBootstrapServiceContract.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPInterface.php to edit this template
+ */
+
+namespace Seblhaire\Formsbootstrap;
+
+/**
+ *
+ * @author seb
+ */
+interface FormsBootstrapServiceContract {
+    public function bsOpen($data);
+    public function bsHidden($data);
+    public function bsText($data);
+    public function bsNumber($data);
+    public function bsRange($data);
+    public function bsTextarea($data);
+    public function bsEmail($data = []);
+    public function pass($data);
+    public function bsPassword($data);
+    public function bsPasswordWithConfirm($data);
+    public function bsSelect($data);
+    public function bsCheckbox($data);
+    public function bsRadio($data);
+    public function bsSubmit($data);
+    public function bsButton($data);
+    public function bsColorpicker($data);
+    public function bsEditor($data);
+    public function bsClose();
+}
--- src/FormsBootstrapServiceProvider.php
+++ src/FormsBootstrapServiceProvider.php
@@ -3,72 +3,50 @@
 namespace Seblhaire\Formsbootstrap;
 
 use Illuminate\Support\ServiceProvider;
-use \Collective\Html\FormFacade as Form;
+
+//use \Collective\Html\FormFacade as Form;
 
 /**
  * Provides extension of Laravelcollective forms for bootstrap css styles
  * @method Form::bsOpen(array $data) mandatory ['id']. prints <form> cf config('formsbootstrap')
- * @method Form::hidden(string $id, int|string $value, (int|string[] $options)) <input type="hidden">
- * @method Forms::bcClose() prints </form>
- * @method Forms::bsText(array $data) mandatory ['name', 'labeltext] prints <input type="text"> cf config('formsbootstrap')
- * @method Forms::bsTextarea(array $data) mandatory ['name', 'labeltext] prints <textarea> cf config('formsbootstrap')
- * @method Forms::bsEmail(array $data) prints <input type="email"> cf config('formsbootstrap')
- * @method Forms::bsPassword(array $data) prints<input type="password"> cf config('formsbootstrap')
- * @method Forms::bsPasswordWithConfirm(array $data) print complete change password, optional old_password and password show/hide button and assword generate button. cf config('formsbootstrap')
- * @method Forms::bsSelect(array $data) mandatory name prints <select><option> cf config('formsbootstrap')
- * @method Forms::bsCheckbox(array $data) mandatory ['name', 'values'] prints  <input type="checkbox"> cf config('formsbootstrap')
- * @method Forms::bsRadio(array $data) mandatory ['name', 'values'] prints <input type="radio"> cf config('formsbootstrap')
- * @method Forms::bsSubmit(array $data) prints <input type="submit"> cf config('formsbootstrap')
- * @method Forms::bsButton(array $data) mandatory ['id', 'label'] prints <input type="button"> cf config('formsbootstrap')
+ * @method Form::bsHidden(string $id, int|string $value, (int|string[] $options)) <input type="hidden">
+ * @method Form::bcClose() prints </form>
+ * @method Form::bsText(array $data) mandatory ['name', 'labeltext] prints <input type="text"> cf config('formsbootstrap')
+ * @method Form::bsTextarea(array $data) mandatory ['name', 'labeltext] prints <textarea> cf config('formsbootstrap')
+ * @method Form::bsEmail(array $data) prints <input type="email"> cf config('formsbootstrap')
+ * @method Form::bsPassword(array $data) prints<input type="password"> cf config('formsbootstrap')
+ * @method Form::bsPasswordWithConfirm(array $data) print complete change password, optional old_password and password show/hide button and assword generate button. cf config('formsbootstrap')
+ * @method Form::bsSelect(array $data) mandatory name prints <select><option> cf config('formsbootstrap')
+ * @method Form::bsCheckbox(array $data) mandatory ['name', 'values'] prints  <input type="checkbox"> cf config('formsbootstrap')
+ * @method Form::bsRadio(array $data) mandatory ['name', 'values'] prints <input type="radio"> cf config('formsbootstrap')
+ * @method Form::bsSubmit(array $data) prints <input type="submit"> cf config('formsbootstrap')
+ * @method Form::bsButton(array $data) mandatory ['id', 'label'] prints <input type="button"> cf config('formsbootstrap')
  */
-class FormsBootstrapServiceProvider extends ServiceProvider
-{
+class FormsBootstrapServiceProvider extends ServiceProvider {
 
     /**
-     * https://laravelcollective.com/docs/5.4/html
-     *
      * @return void
      */
-    public function boot(){
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'formsbootstrap');
-        Form::component('bsOpen', 'formsbootstrap::open', ['data']);
-        Form::component('bsHidden', 'formsbootstrap::hidden', ['data']);
-        Form::component('bsText', 'formsbootstrap::text', ['data']);
-        Form::component('bsNumber', 'formsbootstrap::number', ['data']);
-        Form::component('bsRange', 'formsbootstrap::range', ['data']);
-	      Form::component('bsTextarea', 'formsbootstrap::textarea', ['data']);
-        Form::component('bsEmail', 'formsbootstrap::email', ['data']);
-        Form::component('pass', 'formsbootstrap::pass', ['data']);
-        Form::component('bsPassword', 'formsbootstrap::password', ['data']);
-        Form::component('bsPasswordWithConfirm', 'formsbootstrap::password-with-confirm', ['data']);
-        Form::component('bsSelect', 'formsbootstrap::select', ['data']);
-        Form::component('bsCheckbox', 'formsbootstrap::checkbox', ['data']);
-        Form::component('bsRadio', 'formsbootstrap::radio', ['data']);
-        Form::component('bsSubmit', 'formsbootstrap::submit', ['data']);
-        Form::component('bsButton', 'formsbootstrap::button', ['data']);
-        Form::component('bsColorpicker', 'formsbootstrap::colorpicker', ['data']);
-        Form::component('bsEditor', 'formsbootstrap::editor', ['data']);
-        Form::macro('bsClose', function(){
-            return '</form>';
-        });
-        $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'formsbootstrap');
+    public function boot() {
+        $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'formsbootstrap');
         $this->publishes([
-            __DIR__.'/../config' => config_path('vendor/seblhaire'),
-            __DIR__.'/../resources/lang' => resource_path('lang/vendor/seblhaire/formsbootstrap'),
-            __DIR__.'/../resources/views' => resource_path('views/vendor/seblhaire/formsbootstrap'),
-            __DIR__.'/../resources/js' => resource_path('js/vendor/seblhaire/formsbootstrap'),
-            __DIR__.'/../resources/css' => resource_path('css/vendor/seblhaire/formsbootstrap'),
+            __DIR__ . '/../config' => config_path('vendor/seblhaire'),
+            __DIR__ . '/../resources/lang' => resource_path('lang/vendor/seblhaire/formsbootstrap'),
+            __DIR__ . '/../resources/js' => resource_path('js/vendor/seblhaire/formsbootstrap'),
+            __DIR__ . '/../resources/css' => resource_path('css/vendor/seblhaire/formsbootstrap'),
         ]);
     }
 
-    public function register(){
-      $this->mergeConfigFrom(
-          __DIR__.'/../config/formsbootstrap.php', 'formsbootstrap'
-      );
+    public function register() {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/formsbootstrap.php', 'formsbootstrap'
+        );
+        $this->app->singleton('FormsBootstrapService', function ($app) {
+            return new FormsBootstrapService();
+        });
     }
 
-    public function provides(){
-        return [];
+    public function provides() {
+        return [FormsBootstrapServiceContract::class];
     }
-
 }
