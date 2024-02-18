d1a7e242aac24b369f1127ed9cd19b8801bc860b
Sébastien L'haire <sebastien@lhaire.org>
2024-02-18 12:14:24 +0100

    fix checkboxes

--- resources/views/pass.blade.php
+++
@@ -1,5 +0,0 @@
-<input type="password" name="{!! $data['name']!!}" id="{!! $data['id']!!}" class="{!! $data['inputclass'] !!}"
-@foreach ($data['attributes']  as $key => $value)
-{!! $key !!}="{!! addslashes($value) !!}"
-@endforeach
-/>
--- resources/views/submit.blade.php
+++
@@ -1,6 +0,0 @@
-<?php
-use \Seblhaire\Formsbootstrap\FormsBootstrapUtils;
-
-$data = FormsbootstrapUtils::mergeValues(config('formsbootstrap.defaults.submit'), $data);
- ?>
-{!! Form::submit(FormsBootstrapUtils::translateOrPrint($data['label']), array_merge(['id' => $data['id'], 'class' => $data['class']], $data['attributes'])) !!}
